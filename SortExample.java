3941029859d2ed2108e2c7c7b3b67ebbf439d6f9
Mussarrat K <kitturmussarrat@gmail.com>
2025-10-12 02:17:24 +0530

    Create SortExample.java

---
+++ SortExample.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 1, 4, 2, 8};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
